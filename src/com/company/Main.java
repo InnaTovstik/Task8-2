11b7cf9be66965b51f6320abefd9c183681a83a7
InnaTovstik <kitimail69@gmail.com>
2021-08-13 20:10:09 +0300

    Add print

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,25 +13,24 @@ public class Main {
         catsName.add("Motya");
         catsName.add("Baby");
         catsName.add("Caitlyn");
-        System.out.println(catsName);
-        System.out.println(deleteNode(catsName));
+        System.out.println("Список строк " + catsName);
+        System.out.println("Список строк, из которого удалили все строки, длина которых > 4 " + deleteNode(catsName));
     }
 
     public static LinkedList deleteNode(LinkedList ldl) {
         if (ldl == null || ldl.isEmpty()) {
             return null;
         } else {
-            LinkedList delElemLdl = new LinkedList();
-            for (int i = 0; i < ldl.size(); i++) {
-                String elem = (String) ldl.get(i);
+            LinkedList<String> delElemLdl = new LinkedList<>();
+            for (Object o : ldl) {
+                String elem = (String) o;
                 if (elem.length() > 4) {
-                    delElemLdl.remove(ldl.get(i));
+                    delElemLdl.remove(o);
                 } else {
-                    delElemLdl.add(ldl.get(i));
+                    delElemLdl.add((String) o);
                 }
             }
             return delElemLdl;
         }
-
     }
 }
